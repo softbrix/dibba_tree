bd3a59c7495c57b0d3df70cee3eb814e7de43f59
Andreas Sehr <andreas@mansehr.se>
2017-03-03 18:35:44 +0100

    Dibba tree npm module v0.5.0

---
+++ Readme.md
@@ -0,0 +1,17 @@
+# Dibba tree
+This is an simple tree data structure for javascript.
+
+Create the tree instance by simply requiring the dibba_tree module and create a
+new instance of the object.
+
+Then you can use the insert, update and/or delete methods to modify the  
+of the tree.
+
+The first argument to the insert and update methods is the new content for the node.
+
+The update method supports undefined and null as content whilst the insert
+method strictly forbids this.
+
+## Why Dibba
+Dibba is a town on the eastern coast of the United Arab Emirates. I got the idea
+ of writing this module during a vacation there in the winter of 2017.
---
+++ test/unit.js
@@ -0,0 +1,171 @@
+var assert = require('assert');
+var DibbaTree = require('../index.js');
+
+describe('Dibba tree', function() {
+
+  var testObject1 = { a: 1};
+  var testObject2 = { a: 2};
+  var testObject3 = { a: 3};
+  var testObject4 = { a: 4};
+
+  describe('insert', function() {
+    var tree = new DibbaTree();
+    it('should handle report and error if we try to insert undefined', function() {
+      assert.throws(tree.insert, Error, "Content must not be undefined or null");
+    });
+
+    it('should handle report and error if we try to insert null', function() {
+      assert.throws(function() { tree.insert(null); }, Error, "Content must not be undefined or null");
+    });
+
+    it('should handle insert in the root node', function() {
+      tree.insert(testObject1);
+      assert.equal(testObject1, tree.get());
+    });
+
+    it('should handle insert in a first level path', function() {
+      tree.insert(testObject1, 5);
+      assert.equal(testObject1, tree.get(5));
+    });
+
+    it('should handle insert in a second level path', function() {
+      tree.insert(testObject2, 5, 4);
+      assert.equal(testObject2, tree.get(5, 4));
+    });
+
+    it('should handle insert in a new second level path', function() {
+      tree.insert(testObject3, 2, 3);
+      assert.equal(testObject3, tree.get(2, 3));
+    });
+
+    it('should throw error when reinserting same level', function() {
+      tree.insert(testObject3, 1);
+      assert.equal(testObject3, tree.get(1));
+      assert.throws(function() {tree.insert(testObject1, 1);}, Error, "Node already exists");
+    });
+
+    it('should handle both int and string as key', function() {
+      tree.insert(testObject4, 1, "a");
+      assert.equal(testObject4, tree.get(1, "a"));
+    });
+
+    it('should handle string as key', function() {
+      tree.insert(testObject3, "a", "b");
+      assert.equal(testObject3, tree.get("a", "b"));
+    });
+  });
+
+  describe('insert speed 1 level', function() {
+    var tree = new DibbaTree();
+    it('should be able to create 1000 child nodes in less than one sec', function() {
+      var start = Date.now();
+      var limit = 1000;
+      for(var i = 0; i < limit; ++i) {
+        tree.insert(i, i);
+      }
+      var rootNode = tree.getNode();
+      assert.equal(limit, Object.keys(rootNode.children).length);
+      assert.equal(true, (Date.now() - start) < 1000);
+    });
+  });
+
+  describe('insert speed 2 level', function() {
+    var tree = new DibbaTree();
+    it('should be able to create 10 * 1000 child nodes in less than one sec', function() {
+      var start = Date.now();
+      var limit = 1000;
+      var levels = 10;
+
+      for(var lvl = 0; lvl < levels; ++lvl) {
+        for(var i = 0; i < limit; ++i) {
+          tree.insert(i, lvl, i);
+        }
+      }
+      var rootNode = tree.getNode();
+      assert.equal(levels, Object.keys(rootNode.children).length);
+      assert.equal(true, (Date.now() - start) < 1000);
+    });
+  });
+
+  describe('update', function() {
+    var tree = new DibbaTree();
+    it('should handle update in the root node', function() {
+      tree.insert(testObject1);
+      assert.equal(testObject1, tree.get());
+      tree.update(testObject2);
+      assert.equal(testObject2, tree.get());
+    });
+
+    it('should handle update to undefined in the root node', function() {
+      assert.equal(testObject2, tree.get());
+      tree.update(undefined);
+      assert.equal(undefined, tree.get());
+    });
+
+    it('should handle update to null in the root node', function() {
+      assert.equal(undefined, tree.get());
+      tree.update(null);
+      assert.equal(null, tree.get());
+    });
+
+    it('should handle update in a first level path', function() {
+      var lvl = 5;
+      tree.insert(testObject1, lvl);
+      assert.equal(testObject1, tree.get(lvl));
+      tree.update(testObject3, lvl);
+      assert.equal(testObject3, tree.get(lvl));
+    });
+
+    it('should handle update node content to undefined in a arbitrary path', function() {
+      var lvl = 7;
+      tree.insert(testObject1, lvl);
+      assert.equal(testObject1, tree.get(lvl));
+      tree.update(undefined, lvl);
+      assert.equal(undefined, tree.get(lvl));
+    });
+
+    it('should work the same as insert if node does not exist', function() {
+      var lvl = 12;
+      tree.update(testObject3, lvl);
+      assert.equal(testObject3, tree.get(lvl));
+    });
+
+    it('should return the original object so it can be updated', function() {
+      var lvl = 10;
+      var testObj = {a : 100};
+      tree.insert(testObj, lvl);
+      var treeContent = tree.get(lvl);
+      assert.equal(testObj, treeContent);
+      treeContent.a = 123;
+      assert.equal(123, tree.get(lvl).a);
+    });
+  });
+
+  describe('delete', function() {
+    var tree = new DibbaTree();
+    it('should handle delete in the root node', function() {
+      tree.insert(testObject1);
+      assert.equal(testObject1, tree.get());
+      tree.delete();
+      assert.equal(undefined, tree.get());
+    });
+
+    it('should handle delete in a first level path', function() {
+      var lvl = 5;
+      tree.insert(testObject1, lvl);
+      assert.equal(testObject1, tree.get(lvl));
+      var deletedNode = tree.delete(lvl);
+      assert.equal(testObject1, deletedNode.content);
+      assert.equal(undefined, tree.get(lvl));
+    });
+
+    it('should handle delete in a second level path', function() {
+      var lvl = 5;
+      tree.insert(testObject1, lvl, lvl);
+      assert.equal(testObject1, tree.get(lvl, lvl));
+      var deletedNode = tree.delete(lvl, lvl);
+      assert.equal(testObject1, deletedNode.content);
+      assert.equal(undefined, tree.get(lvl, lvl));
+    });
+  });
+});
